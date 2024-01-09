4afd58fd941d31315481325f9056622deb469888
bambiru <dj70083798@gmail.com>
2024-01-09 11:32:39 +0900

    test

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
+const swiper = new Swiper('.swiper', {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
+
+const swiper2 = new Swiper('.swiper2', {
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper2-button-next',
+    prevEl: '.swiper2-button-prev',
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
